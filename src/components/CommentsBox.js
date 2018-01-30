2ae767e1d72ae1ecc3b53b700a82ecbe0b83001c
Eason Peng <pengyc@rc.inesa.com>
2018-01-30 11:24:03 +0800

    add new components

--- src/components/CommentsBox.js
+++ src/components/CommentsBox.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Avator from './Avator'
 
 class CommentsBox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {value:''}
+        this.handleText = this.handleText.bind(this)
+    }
+    handleText(event){
+        this.setState({value: event.target.value})
+        console.log(this.state.value)
+    }
     render() {
         return (
             <div className="comments-box">
@@ -11,7 +20,7 @@ class CommentsBox extends Component {
                 <div className="comments-box-content">
                     <form action="tt">
                         <div className="form-group mb0">
-                            <textarea name="text" rows="3" className="form-control" placeholder="文明社会，理性评论"></textarea>
+                            <textarea name="text" rows="3" className="form-control" placeholder="文明社会，理性评论" onChange={this.handleText}></textarea>
                             <div className="mt15 text-right">
                                 <button type="button" className="hide"></button>
                                 <button className=" btn btn-primary" type="button">发布评论</button>
--- src/index.js
+++ src/index.js
@@ -1,82 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Child1 from './components/Child1';
-import Child2 from './components/Child2';
-import Child_1 from './components/Child_1';
-import Child_2 from './components/Child_2';
 import CommentsContainer from './components/CommentsContainer';
 
-class Parent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            msg: 'start'
-        }
-    }
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                msg: 'end'
-            })
-        }, 5000)
-    }
-    render() {
-        return (
-            <Child1 msg={this.state.msg} />
-        )
-    }
-}
-
-class Parent2 extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            msg: 'start'
-        }
-    }
-    transferMsg(msg){
-        this.setState({
-            msg
-        })
-    }
-    render(){
-        return (
-            <div>
-                <p>Child msg: {this.state.msg} </p>
-                <Child2 transferMsg = { msg => this.transferMsg(msg)} />
-            </div>
-        )
-    }
-}
-
-class Parent3 extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            msg: 'start'
-        }
-    }
-    transferMsg(msg){
-        this.setState({
-            msg
-        })
-    }
-    componentDidUpdate(){
-        console.log('Parent update');
-    }
-    render(){
-        return(
-            <div>
-                <Child_1 transferMsg = { msg => this.transferMsg(msg)} />
-                <Child_2 msg={this.state.msg} /> 
-            </div>
-        )
-    }
-}
-
-
-ReactDOM.render(<CommentsContainer />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<FlavorForm />, document.getElementById('root'));
+registerServiceWorker();
