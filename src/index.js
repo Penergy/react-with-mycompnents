60f937e5d409036237f10a679b1afaa1084c395e
Eason Peng <pengyc@rc.inesa.com>
2018-01-30 17:48:51 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,62 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import FilterableProductTable from './components/FilterableProductTable';
 import registerServiceWorker from './registerServiceWorker';
-import Calculator from './components/Calculator';
-
-function FancyBorder(props) {
-    return (
-        <div className={'FancyBorder FancyBorder-' + props.color}>
-            {props.children}
-        </div>
-    )
-}
-
-function WelcomeDialog() {
-    return (
-        <FancyBorder color="blue">
-            <h1 classNme="Dialog-title">
-                Welcome
-            </h1>
-            <p className="Dialog-message">
-                Thank you for visiting our spacecraft!
-            </p>
-        </FancyBorder>
-    )
-}
-
-function Contacts() {
-    return <div className="Contacts" > Contacts </div>;
-}
-
-function Chat() {
-    return <div className="Chat"> Chat </div>;
-}
-
-function SplitPane(props) {
-    return (
-        <div className="SplitPane">
-            <div className="SplitPane-left">
-                {props.left}
-            </div>
-            <div className="SplitPane-right">
-                {props.right}
-            </div>
-        </div>
-    );
-}
-
-function App() {
-    return (
-        <SplitPane
-            left={
-                <Contacts />
-            }
-            right={
-                <Chat />
-            } />
-    );
-}
 
 ReactDOM.render(
     <App />,
